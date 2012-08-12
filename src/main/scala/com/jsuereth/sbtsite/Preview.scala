5bc971378f48344fa085be07a579a30b90022095
Dave Cleaver <dscleaver@gmail.com>
2012-08-11 21:41:47 -0400

    Code clean up on the Preview code

--- src/main/scala/com/jsuereth/sbtsite/Preview.scala
+++ src/main/scala/com/jsuereth/sbtsite/Preview.scala
@@ -1,11 +1,7 @@
 package com.jsuereth.sbtsite
 
-import sbt.{Path => _, _}
-import Keys._
-import unfiltered.request._
-import unfiltered.response._
-import unfiltered.util.{IO => _, _}
-import java.io.OutputStream
+import sbt._
+import unfiltered.util._
 import SiteKeys._
 
 object Preview {
@@ -13,25 +9,25 @@ object Preview {
   //@TODO Add configuraiton to make server just local
   val settings: Seq[Setting[_]] = Seq(
     previewSite <<= (makeSite, previewFixedPort, previewLaunchBrowser) map { (file, portOption, browser) =>
-      val port = portOption.getOrElse(Port.any)
-      val server = createServer(file, port).start()
-      println("Preview server started on port %d. Press any key to exit.".format(port))
+      val port = portOption getOrElse Port.any
+      val server = createServer(file, port) start()
+      println("Preview server started on port %d. Press any key to exit." format port)
       @annotation.tailrec def waitForKey() {
-        try { Thread.sleep(500) } catch { case _: InterruptedException => () }
+        try { Thread sleep 500 } catch { case _: InterruptedException => () }
         if(System.in.available <= 0)
           waitForKey()
       }
       if(browser)
-        Browser.open("http://localhost:%d/".format(port))
+        Browser open ("http://localhost:%d/" format port)
       waitForKey()
-      server.stop();
-      server.destroy();
+      server stop()
+      server destroy()
     },
     previewFixedPort := Some(4000),
     previewLaunchBrowser := true
   )
 
   def createServer(siteTarget: File, port: Int) =
-    unfiltered.jetty.Http(port).resources(new URL(siteTarget.toURI.toURL, "."))
+    unfiltered.jetty.Http(port) resources (new URL(siteTarget.toURI.toURL, "."))
 
 }
