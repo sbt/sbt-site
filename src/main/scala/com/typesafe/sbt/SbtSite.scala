b9de77109023832b8a90656c4b9962ea0dfae49a
Peter Vlugter <pvlugter@gmail.com>
2012-09-28 14:16:50 +1200

    Add packaging and publishing support

--- src/main/scala/com/typesafe/sbt/SbtSite.scala
+++ src/main/scala/com/typesafe/sbt/SbtSite.scala
@@ -8,6 +8,8 @@ import site.{ JekyllSupport, PamfletSupport, Preview, SphinxSupport }
 object SbtSite extends Plugin {
   object SiteKeys {
     val makeSite = TaskKey[File]("make-site", "Generates a static website for a project.")
+    val packageSite = TaskKey[File]("package-site", "Create a zip file of the website.")
+
     // Helper to point at mappings for the site.
     val siteMappings = mappings in makeSite
     val siteDirectory = target in makeSite
@@ -23,18 +25,15 @@ object SbtSite extends Plugin {
     import SiteKeys._
 
     val settings = Seq(
-      siteMappings := Seq[(File,String)](),
-      siteDirectory <<= target(_ / "site"),
-      siteSourceDirectory <<= sourceDirectory(_ / "site"),
+      siteMappings := Seq.empty,
+      siteDirectory <<= target / "site",
+      siteSourceDirectory <<= sourceDirectory / "site",
       includeFilter in makeSite := "*.html" | "*.css" | "*.png" | "*.jpg" | "*.gif" | "*.js" | "*.swf",
-      siteMappings <++= (includeFilter in makeSite, siteSourceDirectory) map { (incs, dir ) =>
-        dir ** incs x relativeTo(dir)
-      },
-      makeSite <<= (siteDirectory, cacheDirectory in makeSite, siteMappings) map { (dir, cacheDir, maps) =>
-        val concrete = maps map { case (file, target) => (file, dir / target) }
-        Sync(cacheDir / "make-site")(concrete)
-        dir
-      }
+      siteMappings <++= (siteSourceDirectory, includeFilter in makeSite) map selectSubpaths,
+      makeSite <<= (siteDirectory, cacheDirectory in makeSite, siteMappings) map copySite,
+      artifact in packageSite <<= moduleName(siteArtifact),
+      artifactPath in packageSite <<= Defaults.artifactPathSetting(artifact in packageSite),
+      packageSite <<= (makeSite, artifactPath in packageSite, streams) map createSiteZip
     ) ++ Preview.settings
 
     /** Convenience functions to add a task of mappings to a site under a nested directory. */
@@ -54,10 +53,28 @@ object SbtSite extends Plugin {
       SphinxSupport.settings ++ Seq(addMappingsToSiteDir(mappings in SphinxSupport.Sphinx, alias))
     def pamfletSupport(alias: String = ""): Seq[Setting[_]] =
       PamfletSupport.settings ++ Seq(addMappingsToSiteDir(mappings in PamfletSupport.Pamflet, alias))
+
+    def publishSite(): SettingsDefinition = addArtifact(artifact in packageSite, packageSite)
   }
 
   // Note: We include helpers so other plugins can 'plug in' to this one without requiring users to use/configure the site plugin.
   override val settings = Seq(
-    SiteKeys.siteMappings <<= SiteKeys.siteMappings ?? Seq[(File,String)]()
+    SiteKeys.siteMappings <<= SiteKeys.siteMappings ?? Seq.empty
   )
+
+  def selectSubpaths(dir: File, filter: FileFilter): Seq[(File, String)] = Path.selectSubpaths(dir, filter).toSeq
+
+  def copySite(dir: File, cacheDir: File, maps: Seq[(File, String)]): File = {
+    val concrete = maps map { case (file, target) => (file, dir / target) }
+    Sync(cacheDir / "make-site")(concrete)
+    dir
+  }
+
+  def siteArtifact(name: String) = Artifact(name, Artifact.DocType, "zip", "site")
+
+  def createSiteZip(siteDir: File, zipPath: File, s: TaskStreams): File = {
+    IO.zip(Path.allSubpaths(siteDir), zipPath)
+    s.log.info("Site packaged: " + zipPath)
+    zipPath
+  }
 }
